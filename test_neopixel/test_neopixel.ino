f9e5dc015e1932bd02474efe94af68d34462a662
Tiago <riskybomberpt@gmail.com>
2019-03-21 13:58:55 +0000

    Fixing and documenting Neopixels

--- test_neopixel/test_neopixel.ino
+++ test_neopixel/test_neopixel.ino
@@ -1,15 +1,18 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 8
+#define PIN 4
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(2,PIN,NEO_GRB+NEO_KHZ800);
 
 void setup() {
   strip.begin();
   strip.show();
+  strip.setBrightness(64);
+  strip.setPixelColor(0,20,230,20);//LED on the top size
+  strip.setPixelColor(1,230,30,20);//LED on the back size
+  strip.show();// Have to call it after every setPixel or setBrightness
 }
 
 void loop() {
-  strip.setPixelColor(0,160,200,180);
-  strip.setPixelColor(1,230,200,100);
+  
 }
