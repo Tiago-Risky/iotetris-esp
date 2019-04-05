bb2752d0cad2834ae9423193a1bd04fffcbcb90c
Tiago <riskybomberpt@gmail.com>
2019-04-05 12:25:09 +0100

    Updating block_pir
    Now it has it's actual code and not just a copy
    of the block_temp_hum

--- block_pir/block_pir.ino
+++ block_pir/block_pir.ino
@@ -1,76 +1,62 @@
 #include <Wire.h>
-#include "DHT.h"
 #include <avr/sleep.h>
 #include <IoTetris_slave.h>
 
 IoTetris_Slave tetris;
-
 int neopixelValues[2][3] = {{20,20,200},{200,20,20}};
 
 #define INTERRUPT_PIN 2
-
-// DHT Sensor setup
-#define DHTTYPE DHT11 // Sensor type
-const int DHTPin = 7; // Sensor pin
-
-// Initialize DHT sensor.
-DHT dht(DHTPin, DHTTYPE);
 
 // String to be sent via i2c
 static char finalString[20];
 
 // Flag to go to sleep
-boolean flag = false;
+boolean flagMaster = false;
+boolean flagPIR = false;
+
+// People counter
+int counter = 0;
 
 void setup()
 {
-  dht.begin();
-  tetris.start(9, receiveEvent, requestEvent);
+  tetris.start(8, receiveEvent, requestEvent);
   tetris.setNeopixel(5,68,neopixelValues);
   
   Serial.begin(9600);           // start serial for output
   pinMode(LED_BUILTIN, OUTPUT);
-	pinMode(INTERRUPT_PIN, INPUT_PULLUP);
-	digitalWrite(LED_BUILTIN, HIGH);
-	delay(500);
-	Serial.println("IoTetris: Start");
-	delay(500);	//delay needed
+  pinMode(INTERRUPT_PIN, INPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  Serial.println("IoTetris: Start");
+  delay(500); //delay needed
+
+  go_to_sleep();
 }
 
 void loop()
 {
-  readDHT11();
   delay(250);
-  if(flag)
+  if(flagMaster)
   {
-    flag = false;
+    counter = 0;
+    flagMaster = false;
     delay(100);
     go_to_sleep();
   }
-  
-}
-
-void readDHT11(){
-    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t)) {
-    strcpy(finalString,"DHT Sensor Failed");
-  }
-  else{
-    // Computes Heat Index temperature (apparent temperature)
-    float hic = dht.computeHeatIndex(t, h, false);
-    String fstr = String(h) + "h"+ String(hic) + "ta"+String(t) + "tr";
-    strcpy(finalString,fstr.c_str());
-  }
+  if(flagPIR)
+  {
+    counter++;
+    flagPIR=false;
+    delay(100);
+    go_to_sleep();
+  } 
 }
 
 void requestEvent(int howMany)
 {
+  strcpy(finalString,String(counter).c_str());
   Wire.write(finalString);
-  flag = true;
+  flagMaster = true;
 }
 
 void receiveEvent(int howMany)
@@ -80,13 +66,21 @@ void receiveEvent(int howMany)
 
 void go_to_sleep()
 {
-	sleep_enable();
-	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-	digitalWrite(LED_BUILTIN, LOW);
-	delay(500);
-	Serial.println("IoTetris: Going to sleep");
-	delay(100);
-	sleep_cpu();
-	Serial.println("IoTetris: Continuing");
-	digitalWrite(LED_BUILTIN, HIGH);
+  sleep_enable();
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN),wakeUp, RISING);
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+  Serial.println("IoTetris: Going to sleep");
+  delay(100);
+  sleep_cpu();
+  Serial.println("IoTetris: Continuing");
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void wakeUp()
+{
+  sleep_disable();
+  detachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN));
+  flagPIR=true;
 }
